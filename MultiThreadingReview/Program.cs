89e0def6b2ba03415e978b393cc088318a4a7f8f
fsoria <francis.soria@gmail.com>
2025-03-11 02:18:25 +0900

    Added thread Join in multiple worker threads execution with main thread

--- MultiThreadingReview/Program.cs
+++ MultiThreadingReview/Program.cs
@@ -5,17 +5,25 @@ class Program
 {
     static void Main()
     {
+        Thread[] threads = new Thread[3]; // Array to store worker threads
+
         // Create and start multiple worker threads with parameters
-        for (int i = 0; i< 3; i++)
+        for (int i = 0; i < threads.Length; i++)
         {
-            Thread thread = new Thread(PrintMessage);
-            thread.Start(i); // Pass "i" as ana argument
+            threads[i] = new Thread(PrintMessage);
+            threads[i].Start(i + 1);
         }
 
         for (int i = 0; i < 5; i++)
         {
             Console.WriteLine($"Main Thread: {i}");
             Thread.Sleep(500);
+        }
+
+        // Wait for all worker threads to finish
+        foreach (Thread thread in threads)
+        {
+            thread.Join(); // Blocks the main thread until the worker thread finishes
         }
 
         //thread.Join(); // Wait for the thread to finish
